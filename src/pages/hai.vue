23fefad1e6be9bb907e085c1cbbdea17649a1db1
Keisuke Taniguchi <rememberthatk@icloud.com>
2021-07-17 09:44:54 +0900

    [master] enable apollo nuxt

---
+++ src/pages/hai.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+<p>hai</p>
+<li v-for="sup in supers" :key="sup.id">{{ sup.name}}</li>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api"
+import gql from 'graphql-tag'
+
+export default defineComponent({
+  name: "HaiPage",
+  setup(_, ctx) {
+  },
+  apollo: {
+    supers: gql`
+      query getSupers {
+        supers {
+          id
+          name
+        }
+      }
+    `
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
