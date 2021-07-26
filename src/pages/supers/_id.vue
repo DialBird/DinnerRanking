57f2cc4a393b727fc45719bac981d2528f2f6818
Keisuke Taniguchi <rememberthatk@icloud.com>
2021-07-26 20:19:41 +0900

    [master] separate id page

---
+++ src/pages/supers/_id.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container mx-auto my-8">
+    <p class="text-lg font-bold mb-4">{{ id }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  computed,
+  defineComponent,
+  onMounted,
+  useRoute,
+  useRouter,
+} from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'SuperPage',
+  setup() {
+    const route = useRoute()
+    const router = useRouter()
+    const id = computed(() => route.value.params.id)
+
+    onMounted(() => {
+      router.push('/supers')
+    })
+
+    return { id }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/pages/supers/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container mx-auto my-8">
+    <p class="text-lg font-bold mb-4">Supers</p>
+    <p v-if="loading">loading</p>
+    <template v-else>
+      <li v-for="sup in supers" :key="sup.id">{{ sup.id }}: {{ sup.name }}</li>
+      <form @submit.prevent="onSubmit">
+        <div>
+          <label class="block mb-4">New Super</label>
+          <input
+            v-model="form.name"
+            type="text"
+            class="
+              appearance-none
+              border
+              rounded
+              w-96
+              py-2
+              px-3
+              text-gray-700
+              leading-tight
+            "
+          />
+          <button
+            type="submit"
+            class="
+              bg-blue-500
+              hover:bg-blue-700
+              text-white
+              font-bold
+              py-2
+              px-4
+              rounded
+            "
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from '@nuxtjs/composition-api'
+import {
+  GetSupersDocument,
+  InsertSuperDocument,
+} from '@/generated/graphqlOperations'
+import {
+  useMutation,
+  useQuery,
+  useQueryLoading,
+  useResult,
+} from '@vue/apollo-composable'
+
+export default defineComponent({
+  name: 'SupersPage',
+  setup() {
+    const { result, refetch } = useQuery(GetSupersDocument)
+    const { mutate: insertSuper } = useMutation(InsertSuperDocument)
+    const loading = useQueryLoading()
+    const supers = useResult(result, [], (data) => data?.supers)
+
+    const form = ref({
+      name: '',
+    })
+
+    const onSubmit = async () => {
+      await insertSuper({ name: form.value.name })
+      form.value.name = ''
+      refetch()
+    }
+
+    return { form, loading, onSubmit, supers }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
